cda869d4f3ea9a3087d789256761584d99c56cc4
Eduardo Vásquez <eduardovasquezguerra@gmail.com>
2026-07-26 15:04:00 -0300

    implementación de express slow down y rate limit

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -4,12 +4,31 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { isAuth, errorHandler } from "./middlewares/index.js";
 import { authRouter, meRouter, companyRouter } from "./routers/index.js";
+import { slowDown } from "express-slow-down";
+import { rateLimit, MINUTE } from "express-rate-limit";
+
+const rateLimiter = rateLimit({
+  windowMs: 15 * MINUTE, // SECOND, MINUTE, HOUR, and DAY constants are available, or a use bare number for milliseconds
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+const limiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 5, // Allow 5 requests per 15 minutes.
+  delayMs: (hits) => hits * 100, // Add 100 ms of delay to every request after the 5th one.
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(limiter);
+app.use(rateLimiter);
 
 app.use("/auth", authRouter);
 app.use("/me", isAuth, meRouter);
